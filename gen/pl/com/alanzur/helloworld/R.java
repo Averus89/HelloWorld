06f0185847be1de0ee98682748dab16fd3852d35
Alan Żur <alanzur@gmail.com>
2013-02-19 02:01:03 +0100

    Lekcja 11 Bootcamp series 2011

--- gen/pl/com/alanzur/helloworld/R.java
+++ gen/pl/com/alanzur/helloworld/R.java
@@ -10,26 +10,30 @@ package pl.com.alanzur.helloworld;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int alpha_blue=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int editText1=0x7f070002;
-        public static final int menu_settings=0x7f070003;
-        public static final int textView1=0x7f070000;
+        public static final int button_send=0x7f080001;
+        public static final int edit_status=0x7f080002;
+        public static final int menu_settings=0x7f080003;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int menu_settings=0x7f040001;
-        public static final int status_button_text=0x7f040003;
-        public static final int status_text=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int menu_settings=0x7f050001;
+        public static final int status_button_text=0x7f050003;
+        public static final int status_text=0x7f050002;
+        public static final int status_update_hint=0x7f050004;
     }
     public static final class style {
         /** 
@@ -53,10 +57,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/pl/com/alanzur/helloworld/MainActivity.java
+++ src/pl/com/alanzur/helloworld/MainActivity.java
@@ -1,22 +1,78 @@
 package pl.com.alanzur.helloworld;
 
-import android.os.Bundle;
+import winterwell.jtwitter.Twitter;
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	public static String TAG = "MainActivity";
+	Button buttonUpdate;
+	EditText editStatus;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//Debug.startMethodTracing("Yamba.Trace");
+		buttonUpdate = (Button) findViewById(R.id.button_send);
+		editStatus = (EditText) findViewById(R.id.edit_status);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onStop()
+	 */
+	@Override
+	protected void onStop() {
+		//Debug.stopMethodTracing();
+		super.onStop();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public void onClick(View v) {
+		String statusTextString = editStatus.getText().toString();
+
+		new PostToTwitter().execute(statusTextString);
+
+		Log.d("StatusActivity", "onClick with text: " + statusTextString);
+	}
+
+	// input, progress, result
+	class PostToTwitter extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				Twitter twitter = new Twitter("student", "password");
+				twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+				twitter.setStatus(params[0]);
+				Log.d(TAG, "Posted");
+				return "Posted: " + params[0];
+			} catch (Exception e) {
+				Log.e(TAG, "Died: " + e);
+				return "Died: " + params[0];
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+			super.onPostExecute(result);
+		}
+
+	}
 }
